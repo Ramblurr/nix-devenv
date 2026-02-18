c0da97ec131b1822f48e3115d4b15ce0ab20afed
Casey Link <unnamedrambler@gmail.com>
2026-02-18 07:59:55 +0100

    brepl: 2.5.1 -> 2.7.0

--- pkgs/brepl.nix
+++ pkgs/brepl.nix
@@ -8,8 +8,8 @@ callPackage (
   fetchFromGitHub {
     owner = "licht1stein";
     repo = "brepl";
-    rev = "v2.5.1";
-    hash = "sha256-5rv7fFRe32a1JL2BAD3mMP2b3VGtiMC+MDphhJgJkgk=";
+    rev = "v2.7.0";
+    hash = "sha256-eTQS5LvAOLvx46YS6V/2+bYkWSELTTwZ1ir77eLk99M=";
   }
   + "/package.nix"
 ) { }
