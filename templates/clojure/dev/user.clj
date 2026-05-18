b6b7b306fbd33d040abe321434c86cb5064b73d4
Casey Link <casey@outskirtslabs.com>
2026-05-18 11:12:05 +0200

    Use shared portal tap helpers

--- templates/clojure/dev/user.clj
+++ templates/clojure/dev/user.clj
@@ -1,22 +1,25 @@
 (ns user
   (:require
-   #_[portal.api :as p]
-   [clj-reload.core :as clj-reload]))
+   [clj-reload.core :as clj-reload]
+   [ol.dev.portal :as portal]))
 
 ((requiring-resolve 'hashp.install/install!))
 
 (set! *warn-on-reflection* true)
+
+(defonce portal! (portal/open-portals))
 
 ;; Configure the paths containing clojure sources we want clj-reload to reload
 (clj-reload/init {:dirs      ["src" "dev" "test"]
                   :no-reload #{'user 'dev 'ol.dev.portal}})
 
 (comment
-  (defonce ps ((requiring-resolve 'ol.dev.portal/open-portals)))
+  (portal/logs 5)
+  (portal/last-log)
+  (portal/clear-logs!)
 
   (clj-reload/reload)
   (clj-reload/reload {:only :all}) ;; rcf
-  #_(reset! my-portal/portal-state nil)
   (clojure.repl.deps/sync-deps)
   ;;;
   )
