ac8bb2f6886c4d284928fdb6ebab0c69b852f8d4
Casey Link <casey@outskirtslabs.com>
2026-06-11 10:08:46 +0200

    remove garnix-cli from capsule and template

--- capsules/base.nix
+++ capsules/base.nix
@@ -3,10 +3,6 @@
 {
   commands = map (withCategory "base") [
     {
-      package = pkgs.garnix-cli;
-      name = "garnix-cli";
-    }
-    {
       package = pkgs.spdx;
       name = "spdx";
       help = "Manage SPDX licenses and copyright headers";
